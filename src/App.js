d04c7bd305d73db36327906c8aa047b171f0b435
babe124578 <natthapong41@gmail.com>
2019-04-27 14:34:42 +0700

    helloworld

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Startpage from "./Component/Startpage";
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isStart: false
+    };
+  }
+  
+  changeStart(status) {
+    this.setState({
+      isStart: status
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Startpage></Startpage>
+      </div>
+    );
+  }
 }
 
 export default App;
